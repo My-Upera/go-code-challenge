dee49eec8486ab17a3d65f32bc72181778291560
Mohammad Davatgar <42391064+muhammad-davatgar@users.noreply.github.com>
2023-12-05 11:25:22 +0330

    Update migrations.sql

--- shoes/migrations.sql
+++ shoes/migrations.sql
@@ -1,1 +1,1 @@
-CREATE TABLE shoes(shoe_id SERIAL PRIMARY KEY,shoe_name varchar(20),shoe_sizes INT []);
+CREATE TABLE shoes(shoe_id SERIAL PRIMARY KEY,shoe_name varchar(20) NOT NULL,shoe_sizes INT [] NOT NULL ,brand_name varchar(20));
